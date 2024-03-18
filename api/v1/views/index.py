8671b5f34a5ac56bc521ae929a4c6852d6269a4d
Ashley <ajchapman777@gmail.com>
2024-03-18 19:45:57 +0000

    id

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+"""Module to handle index routes
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status of the server
+    """
+    return jsonify({"status": "OK"})
+
+if __name__ == "__main__":
+    pass  # If you need to add additional functionality, you can do it here
+
+
 from api.v1.views import app_views
 from models import storage
 
