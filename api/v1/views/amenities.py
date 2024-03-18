91ac8867fd00d93dea2bc155c45a76243b9c186d
Ashley <ajchapman777@gmail.com>
2024-03-18 20:10:25 +0000

    amenities.py

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Amenities view module
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage, Amenity
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """Creates a Amenity"""
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates a Amenity object"""
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    ignore = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
