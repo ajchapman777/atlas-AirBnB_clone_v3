24220e11fe38f7b52feee894204f6c0b6e2e45b0
Ashley <ajchapman777@gmail.com>
2024-03-18 18:35:44 +0000

    cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-
+"""Module to handle City objects
 """
-City module to handle all RestFul API actions for City objects
-"""
-
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage, State, City
 
-
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_cities_by_state(state_id):
-    """
-    Retrieves the list of all City objects of a State
+    """Retrieves the list of all City objects of a State
     """
     state = storage.get(State, state_id)
     if not state:
@@ -20,13 +15,13 @@ def get_cities_by_state(state_id):
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
-
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
-    """
-    Retrieves a City object
+    """Retrieves a City object by its id
     """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
 
+if __name__ == "__main__":
+    pass  # If you need to add additional functionality, you can do it here
