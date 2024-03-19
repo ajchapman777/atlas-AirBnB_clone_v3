e923d6c8891a1d27ab8f20fb40d95be48fecf830
jessasesh <jessica.dison@yahoo.com>
2024-03-19 02:26:26 +0000

    Changed file perms & updated status method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
-def get_status():
+def get_stats():
     """Returns the status of the server
     """
     return jsonify({"status": "OK"})
@@ -18,9 +18,10 @@ from api.v1.views import app_views
 from models import storage
 
 @app_views.route('/status')
-def status():
+def stats():
     """
     Route that returns status
     """
-    "status": "OK"
-    return
+    return jsonify({
+       "status": "OK"
+       })
