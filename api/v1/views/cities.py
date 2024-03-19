b10b1145046e6d353e420ec56c0aa01570bd04ee
Ashley <ajchapman777@gmail.com>
2024-03-19 17:48:54 +0000

    cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,9 @@ from api.v1.views import app_views
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_cities(state_id):
-    """Retrieves the list of all City objects of a State."""
+    """
+    Retrieves the list of all City objects of a State.
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -20,7 +22,9 @@ def get_cities(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
-    """Retrieves a City object."""
+    """
+    Retrieves a City object.
+    """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -29,7 +33,9 @@ def get_city(city_id):
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
-    """Deletes a City object."""
+    """
+    Deletes a City object.
+    """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -40,7 +46,9 @@ def delete_city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def create_city(state_id):
-    """Creates a City."""
+    """
+    Creates a City.
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
