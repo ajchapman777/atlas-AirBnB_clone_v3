9fd42fc7a5e08842a0e84f4c67f5cdd7310b7065
jessasesh <jessica.dison@yahoo.com>
2024-03-19 19:56:10 +0000

    PEP correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,19 +37,21 @@ class FileStorage:
     def count(self, cls=None):
         """Count the number of objects in storage."""
         if cls:
-            return len([obj for obj in self.__objects.values() if isinstance(obj, cls)])
+            return len([obj for obj in self.__objects.values()
+                        if isinstance(obj, cls)])
         else:
             return len(self.__objects)
 
     def all(self, cls=None):
-            """returns the dictionary __objects"""
-            if cls is not None:
-                new_dict = {}
-                for key, value in self.__objects.items():
-                    if cls == value.__class__ or cls == value.__class__.__name__:
-                        new_dict[key] = value
-                        return new_dict
-                    return self.__objects
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
