007d14f89fe244cf7328f482d757c6089084c97a
Ashley <ajchapman777@gmail.com>
2024-03-19 16:44:31 +0000

    amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,6 +5,7 @@ from flask import jsonify, abort, request
 from models import storage, Amenity
 from api.v1.views import app_views
 
+
 @app_views.route('/amenities', methods=['GET'])
 def get_all_amenities():
     """Retrieves the list of all Amenity objects."""
@@ -14,17 +15,19 @@ def get_all_amenities():
     ]
     return jsonify(amenities)
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_amenity(amenity_id):
-    """Retrieves a Amenity object."""
+    """Retrieves a Amenity object by its ID."""
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
     return jsonify(amenity.to_dict())
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def delete_amenity(amenity_id):
-    """Deletes a Amenity object."""
+    """Deletes a Amenity object by its ID."""
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
@@ -32,9 +35,10 @@ def delete_amenity(amenity_id):
     storage.save()
     return jsonify({})
 
+
 @app_views.route('/amenities', methods=['POST'])
 def create_amenity():
-    """Creates a Amenity."""
+    """Creates a new Amenity object."""
     if not request.json:
         abort(400, description="Not a JSON")
     if 'name' not in request.json:
@@ -44,9 +48,10 @@ def create_amenity():
     amenity.save()
     return jsonify(amenity.to_dict()), 201
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def update_amenity(amenity_id):
-    """Updates a Amenity object."""
+    """Updates a Amenity object by its ID."""
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
