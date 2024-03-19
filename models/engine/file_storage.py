23dd52ab9f3a69319437e402ef24bcd32eca1929
jessasesh <jessica.dison@yahoo.com>
2024-03-19 02:02:22 +0000

    Correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,20 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """
-        Returns the object based on the class and its ID, or None if not found.
-        """
-        if cls and id:
-            getter = f"{cls.__name__}.{id}"
-            return self.all(cls).get(getter)
-        return None
-
-    def count(self, cls=None):
-        """
-        Returns the number of objects in storage matching the given class. 
-        If no class is passed, returns the count of all objects in storage.
-        """
-        if cls is None:
-            return len(self.all(cls))
