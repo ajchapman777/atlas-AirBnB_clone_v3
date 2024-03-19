b061723f72b6e89b9ed01f913a5d2559746351ca
Ashley <ajchapman777@gmail.com>
2024-03-19 20:23:24 +0000

    amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,8 +4,6 @@
 from flask import jsonify, abort, request
 from models.amenity import Amenity
 from models import storage
-from models.city import City
-from models.state import State
 from api.v1.views import app_views
 
 
