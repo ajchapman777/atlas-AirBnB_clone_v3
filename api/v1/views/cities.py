08b640719b0c0c1f62ea6cea0aea56e14241c1a1
Ashley <ajchapman777@gmail.com>
2024-03-19 18:22:37 +0000

    cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Defines routes for cities objects.
+Creates cities, etc..
 """
 
 from flask import jsonify, abort, request
-from models import storage, City, State
+from models.state import State
+from models.city import City
+from models import storage
 from api.v1.views import app_views
 
 
