f780f9216d773c13c9d22cab3b77d791992daa1d
Ashley <ajchapman777@gmail.com>
2024-03-20 15:03:26 +0000

    places_reviews.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,66 +1,104 @@
 #!/usr/bin/python3
 """
-View for states
+This module creates the view for all state objects
+and handles all default api actions
 """
-from flask import jsonify, request, abort
+
+from api.v1.views import app_views, index
+from flask import Flask, abort, jsonify, request
 from models import storage
 from models.state import State
-from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
-    """Retrieve the list of all State objects"""
-    states = [state.to_dict() for state in storage.all(State).values()]
-    return jsonify(states)
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def get_all_states():
+    """
+    This method retrieves a list of all state objects
+    Args: states_all - list of all states, keys excluded
+          states_json - all states converted to a list of dictionaries
+    Return: a json dictionary containing all state objects
+    """
+    states_all = storage.all(State).values()
+    # print(f"states_all type: {type(states_all)}")
+    states_json = [state.to_dict() for state in states_all]
+    return jsonify(states_json)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def get_state(state_id):
-    """Retrieve a State object"""
+    """
+    This method retrieves one state object
+    Args: state - retrieves one state object, based on its state id
+          state_json - state object converted to a dictionary
+    Return: a json dictionary containing one state object
+    """
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
+        abort(404)  # Bad request
+    state_json = state.to_dict()
+    return jsonify(state_json)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route(
+        "/states/<state_id>", methods=["DELETE"], strict_slashes=False)
 def delete_state(state_id):
-    """Delete a State object"""
+    """
+    This method deletes one state object
+    Args: state - retrieves one state object, based on its state id
+    Return: an empty json dictionary
+    """
+    # print("here")
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
-    state.delete()
+        abort(404)  # Bad request
+    storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return jsonify({}), 200  # OK
+    # ^^^ not 200? -Ace
+    # Nvm yeah I went ahead and changed it. -Ace
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
-    """Create a State object"""
-    data = request.get_json()
-    if data is None:
-        abort(400, 'Not a JSON')
-    if 'name' not in data:
-        abort(400, 'Missing name')
-    state = State(**data)
+    """
+    This method creates a state object
+    Args: state - gets an HTTP body request to a state object
+          json_data - holds the json data request
+    Return: a json dictionary containing one state object
+    """
+    json_data = request.get_json(silent=True)
+    if not json_data:
+        # print("here")
+        abort(400, "Not a JSON")  # Bad request
+    if "name" not in json_data:
+        abort(400, "Missing name")  # Bad request
+    state = State(**json_data)
     state.save()
-    return jsonify(state.to_dict()), 201
+    state_json = state.to_dict()
+    return jsonify(state_json), 201  # OK
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route(
+        "/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
-    """Update a State object"""
+    """
+    This method updates a state object
+    Args: state - retrieves one state object, based on its state id
+          json_data - json request data for readability
+          state_json - state object converted to a dictionary
+    Return:
+    """
     state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    data = request.get_json()
-    if data is None:
-        abort(400, 'Not a JSON')
-    # Update state object with key-value pairs from the dictionary
-    for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+    json_data = request.get_json(silent=True)
+    if not state:
+        abort(404)  # Bad request
+    if not json_data:
+        abort(400, "Not a JSON")  # Bad request
+    if "name" not in json_data:
+        abort(400, "Missing name")  # Bad request
+    for key, value in json_data.items():
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
     state.save()
-    return jsonify(state.to_dict()), 200
+    state_json = state.to_dict()
+    return jsonify(state_json)
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,105 +1,101 @@
 #!/usr/bin/python3
-"""Defines API routes for User objects."""
+"""
+This module creates the view for all state objects
+and handles all default api actions
+"""
 
-from api.v1.views import app_views
-from flask import jsonify, abort, make_response, request
+from api.v1.views import app_views, index
+from flask import Flask, abort, jsonify, request
 from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'])
-def get_users():
-    """Retrieves the list of all User objects."""
-    users = [user.to_dict() for user in storage.all(User).values()]
-    return jsonify(users)
+@app_views.route("/users", methods=["GET"], strict_slashes=False)
+def get_all_users():
+    """
+    This method retrieves a list of all state objects
+    Args: users - list of all users, keys excluded
+          users_json - all users converted to a list of dictionaries
+    Return: a json dictionary containing all user objects
+    """
+    users = storage.all(User).values()
+    users_json = [user.to_dict() for user in users]
+    return jsonify(users_json)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+@app_views.route("/users/<user_id>", methods=["GET"], strict_slashes=False)
 def get_user(user_id):
-    """Retrieves a User object by its ID.
-
-    Args:
-        user_id (str): The ID of the user to retrieve.
-
-    Returns:
-        JSON: A JSON representation of the user object.
-
-    Raises:
-        404: If no user with the given ID is found.
+    """
+    This method retrieves one user object
+    Args: user - retrieves one user object, based on its user id
+          user_json - user object converted to a dictionary
+    Return: a json dictionary containing one user object
     """
     user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-    return jsonify(user.to_dict())
+    if user is None:
+        abort(404)  # Bad request
+    user_json = user.to_dict()
+    return jsonify(user_json)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route("/users/<user_id>", methods=["DELETE"], strict_slashes=False)
 def delete_user(user_id):
-    """Deletes a User object by its ID.
-
-    Args:
-        user_id (str): The ID of the user to delete.
-
-    Returns:
-        JSON: An empty dictionary.
-
-    Raises:
-        404: If no user with the given ID is found.
+    """
+    This method deletes one user object
+    Args: user - retrieves one user object, based on its user id
+    Return: an empty json dictionary
     """
     user = storage.get(User, user_id)
-    if not user:
-        abort(404)
+    if user is None:
+        abort(404)  # Bad request
     storage.delete(user)
+    print(f"type of user: {type(user)}")
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200  # OK
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route("/users", methods=["POST"], strict_slashes=False)
 def create_user():
-    """Creates a new User object.
-
-    Returns:
-        JSON: A JSON representation of the newly created user object.
-
-    Raises:
-        400: If the request body is not a valid JSON or if required
-        fields are missing.
     """
-    if not request.json:
-        abort(400, description="Not a JSON")
-    if 'email' not in request.json:
-        abort(400, description="Missing email")
-    if 'password' not in request.json:
-        abort(400, description="Missing password")
-    data = request.get_json()
-    user = User(**data)
-    user.save()
-    return jsonify(user.to_dict()), 201
+    This method creates a user object
+    Args: user - gets an HTTP body request to a user object
+          json_data - holds the json data request
+          user_json - holds the dictionary of a user object
+    Return: a json dictionary containing one user object
+    """
+    json_data = request.get_json(silent=True)
+    if not json_data:
+        abort(400, "Not a JSON")
+    if "email" not in json_data:
+        abort(400, "Missing email")
+    if "password" not in json_data:
+        abort(400, "Missing password")
+    user = User(**json_data)
+    storage.new(user)
+    storage.save()
+    user_json = user.to_dict()
+    return jsonify(user_json), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
 def update_user(user_id):
-    """Updates a User object by its ID.
-
-    Args:
-        user_id (str): The ID of the user to update.
-
-    Returns:
-        JSON: A JSON representation of the updated user object.
-
-    Raises:
-        404: If no user with the given ID is found.
-        400: If the request body is not a valid JSON.
+    """
+    This method updates a user object
+    Args: user - retrieves one user object, based on its user id
+          json_data - json request data for readability
+          user_json - user object converted to a dictionary
+    Return: a json dictionary
     """
     user = storage.get(User, user_id)
     if not user:
-        abort(404)
-    if not request.json:
-        abort(400, description="Not a JSON")
-    data = request.get_json()
-    ignore = ['id', 'email', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignore:
+        abort(404)  # Bad request
+    json_data = request.get_json(silent=True)
+    if not json_data:
+        abort(400, "Not a JSON")  # Bad request
+    leave_out_keys = ["id", "email", "created_at", "updated_at"]
+    for key, value in json_data.items():
+        if key not in leave_out_keys:
             setattr(user, key, value)
-    user.save()
-    return jsonify(user.to_dict()), 200
+    storage.save()
+    user_json = user.to_dict()
+    return jsonify(user_json), 200  # OK
