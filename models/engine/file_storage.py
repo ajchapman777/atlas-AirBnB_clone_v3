c23282c6a3831e49e0269eca3e6fd0c80af50a0b
Ashley <ajchapman777@gmail.com>
2024-03-20 19:20:25 +0000

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,27 +41,26 @@ class FileStorage:
             self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
+    """Serializes __objects to the JSON file (path: __file_path)"""
     json_objects = {}
     for key in self.__objects:
         json_objects[key] = self.__objects[key].to_dict()
     with open(self.__file_path, 'w') as f:
         json.dump(json_objects, f)
 
-    def reload(self):
-        """Deserializes the JSON file to __objects"""
-        try:
-            with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-                for key in jo:
-                    self.__objects[key] = classes[jo[key]
-                            ["__class__"]](**jo[key])
-        except FileNotFoundError:
-            # Handle the FileNotFoundError
-            pass
-        except Exception as e:
-            # Handle other exceptions
-            print("An error occurred during file reload:", e)
+        def reload(self):
+            """Deserializes the JSON file to __objects"""
+            try:
+                with open(self.__file_path, 'r') as f:
+                    jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+    except FileNotFoundError:
+        # Handle the FileNotFoundError
+        pass
+    except Exception as e:
+        # Handle other exceptions
+        print("An error occurred during file reload:", e)
 
     def delete(self, obj=None):
         """delete obj from __objects if it's inside"""
