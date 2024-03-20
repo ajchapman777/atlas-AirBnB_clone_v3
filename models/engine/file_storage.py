cb5b6dba38f37c19f39cc7d866925dc31c2a0850
Ashley <ajchapman777@gmail.com>
2024-03-20 15:39:16 +0000

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,13 +48,14 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
-                def reload(self):
-                    """Deserializes the JSON file to __objects"""
-                    try:
-                        with open(self.__file_path, 'r') as f:
+            def reload(self):
+                """Deserializes the JSON file to __objects"""
+                try:
+                    with open(self.__file_path, 'r') as f:
                             jo = json.load(f)
                             for key in jo:
-                                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                                self.__objects[key] = classes[jo[key]["__class__"]]
+                                (**jo[key])
                     except FileNotFoundError:
                         # Handle the FileNotFoundError
                         pass
