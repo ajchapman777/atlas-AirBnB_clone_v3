83c74148bccb25b890d7d8b0750a27f5425200c2
Ashley <ajchapman777@gmail.com>
2024-03-20 14:58:01 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""
-This module initializes the Flask application
-and sets up routes for the API.
-"""
+"""This module defines a flask web app"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 from os import getenv
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(self):
-    """
-    Method that calls close
-    """
+def teardown(exception):
+    """This method handles the teardown, on itself"""
     storage.close()
 
 
-if __name__ == '__main__':
-    try:
-        host = os.environ.get('HBNB_API_HOST')
-    except:
-        host = '0.0.0.0'
+@app.errorhandler(404)
+def error_404(exception):
+    """ return for 404 errors """
+    return jsonify({"error": "Not found"}), 404
 
-    try:
-        port = os.environ.get('HBNB_API_PORT')
-    except:
-        port = '5000'
 
-    app.run(host=host, port=port)
+def start_flask():
+    """
+    starts flask - sets host and port from getenv
+    if 'name == main' runs this method
+    """
+    app.run(host=getenv('HBNB_API_HOST', default='localhost'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True, debug=True)
+
+
+if __name__ == "__main__":
+    start_flask()
